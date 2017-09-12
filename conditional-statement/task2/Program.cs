b51820ec5ad2346883e691584225e46e7a3886ef
inkemi <mt.inkeroinen@gmail.com>
2017-09-12 14:03:36 +0300

    Even or odd

--- conditional-statement/task2/Program.cs
+++ conditional-statement/task2/Program.cs
@@ -6,7 +6,47 @@ namespace task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Prompt user
+            Console.Write("Please enter a number: ");
+
+            // Read user input
+            string userInput;
+            userInput = Console.ReadLine();
+
+            // Evaluate user input
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            int modulo = evaluatedNumber / 2;
+
+            // If userInput is 0
+
+            if (evaluatedNumber == 0)
+            {
+                Console.WriteLine("Number {0} is even", evaluatedNumber);
+
+            }
+
+            // If modulo = 0
+
+            else if (modulo == 0)
+            {
+                Console.WriteLine("Number {0} is even", evaluatedNumber);
+
+            }
+
+            // If modulo > or > 0
+
+            else
+            {
+                Console.WriteLine("Number {0} is odd", evaluatedNumber);
+
+            }
+
+            // Wait for user input
+
+            Console.ReadKey();
         }
     }
 }
