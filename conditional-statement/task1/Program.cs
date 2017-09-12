03a830f1ceba84252dcc05355b8f82c2eceb566e
inkemi <mt.inkeroinen@gmail.com>
2017-09-12 13:15:38 +0300

    Compare training

---
+++ conditional-statement/task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Prompt user
+            Console.Write("Please enter a number: ");
+
+            // Read user input
+            string userInput;
+            userInput = Console.ReadLine();
+
+            // Evaluate user input
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            // IF > 0
+            if (evaluatedNumber > 0)
+            {
+                Console.WriteLine("Number {0} is greater than 0", evaluatedNumber);
+            }
+
+            // IF < 0
+            else if(evaluatedNumber < 0)
+            {
+                Console.WriteLine("Number {0} is less than 0", evaluatedNumber);
+            }
+
+            // IF == 0
+
+            else
+            {
+                Console.WriteLine("Number {0} equals 0", evaluatedNumber);
+            }
+
+            // Console.WriteLine("User input was: string: {0} : integer: {1}", userInput , evaluatedNumber);
+            // Console.WriteLine("User input was: " + userInput);
+
+            // Wait for user input
+
+            Console.ReadKey();
+
+        }
+    }
+}
