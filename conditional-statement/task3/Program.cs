c910ffbeb97dfa4c045ba9c004aad428c64ed100
inkemi <mt.inkeroinen@gmail.com>
2017-09-12 14:15:32 +0300

    Combinated task1 and task2

---
+++ conditional-statement/task3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user
+            Console.Write("Please enter a number: ");
+
+            // Read user input
+            string userInput;
+            userInput = Console.ReadLine();
+
+            // Evaluate user input
+
+            int evaluatedNumber;
+            int.TryParse(userInput, out evaluatedNumber);
+
+            int modulo = evaluatedNumber / 2;
+
+            // If userInput is 0
+
+            if (evaluatedNumber == 0)
+            {
+                Console.WriteLine("Number {0} equals even and 0", evaluatedNumber);
+
+            }
+
+            // If modulo = 0
+
+            else if (modulo == 0 && evaluatedNumber > 0)
+            {
+                Console.WriteLine("Number {0} is even and positive", evaluatedNumber);
+
+            }
+
+
+            else if (modulo == 0 && evaluatedNumber < 0)
+            {
+                Console.WriteLine("Number {0} is even and negative", evaluatedNumber);
+
+            }
+
+            // If modulo > or > 0
+
+
+            else if (modulo != 0 && evaluatedNumber > 0)
+            {
+                Console.WriteLine("Number {0} is odd and positive", evaluatedNumber);
+
+            }
+
+            else
+            {
+                Console.WriteLine("Number {0} is odd and negative", evaluatedNumber);
+
+            }
+
+            // Wait for user input
+
+            Console.ReadKey();
+        }
+    }
+}
