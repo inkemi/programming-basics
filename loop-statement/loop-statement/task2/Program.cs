36244c715abdb74177f373acbd261a30579aaabd
inkemi <mt.inkeroinen@gmail.com>
2017-09-13 14:01:56 +0300

    Loop exercise task1 and task2

---
+++ loop-statement/loop-statement/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Give N: ");
+            string userInput1;
+            userInput1 = Console.ReadLine();
+
+            int n;
+            int.TryParse(userInput1, out n);
+
+            if (n < 0)
+            {
+                Console.WriteLine("N is {0} and undefined", n);
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine("N is {0} and result is 0", n);
+            }
+            else if (n > 0)
+            {
+                int result;
+                result = 0;
+                int result2;
+                result2 = 1;
+                int i;
+                i = 1;
+                for (i = 1; i <= n; i++)
+                {
+                    result = result + i;
+                    Console.WriteLine("{0} + {1} = {2}", result2, i, result);
+                    result2 = result;
+                }
+                Console.WriteLine("N is {0} and result is {1}", n, result);
+            }
+            else
+            {
+                Console.WriteLine("N is {0} and it is something wrong with it.", n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
